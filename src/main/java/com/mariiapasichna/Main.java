87c63f10ddde4d5e7d7189c18001c8c8e599e5d9
Mariia Pasichna <pasichnayam@gmail.com>
2020-04-22 16:01:54 +0300

    add printResponse

--- src/main/java/com/mariiapasichna/Main.java
+++ src/main/java/com/mariiapasichna/Main.java
@@ -3,19 +3,27 @@ package com.mariiapasichna;
 import com.mariiapasichna.api.UserApi;
 import com.mariiapasichna.model.User;
 
+import javax.ws.rs.core.Response;
+
 public class Main {
 
     public static final String URL = "http://localhost:" + System.getProperty("config") + "/Lesson_24_mariiapasichna/database";
     public static final String PATH = "clear";
 
     public static void main(String[] args) {
-        UserApi.testDeleteAll(PATH);
-        UserApi.testCreate(new User("Alex", 22));
-        UserApi.testCreate(new User("Ben", 33));
-        UserApi.testCreate(new User("Bill", 44));
-        UserApi.testCreate(new User("Poll", 55));
-        UserApi.testGetList();
-        UserApi.testGet("31");
-        UserApi.testUpdate(new User(7, "Poll", 100));
+        UserApi userApi = UserApi.getInstance();
+        printResponse(userApi.testDeleteAll(PATH));
+        printResponse(userApi.testCreate(new User("Alex", 22)));
+        printResponse(userApi.testCreate(new User("Ben", 33)));
+        printResponse(userApi.testCreate(new User("Bill", 44)));
+        printResponse(userApi.testCreate(new User("Poll", 55)));
+        printResponse(userApi.testGetList());
+        printResponse(userApi.testGet("75"));
+        printResponse(userApi.testUpdate(new User(75, "Poll", 100)));
+        //printResponse(userApi.testDelete(new User(71, "Poll", 55)));
+    }
+
+    private static void printResponse(Response response) {
+        System.out.println("Response code: " + response.getStatus() + ". Response body: " + response.readEntity(String.class));
     }
 }
--- src/main/java/com/mariiapasichna/api/UserApi.java
+++ src/main/java/com/mariiapasichna/api/UserApi.java
@@ -12,50 +12,46 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 public class UserApi {
-    static WebTarget getWebTarget() {
+    private static UserApi instance;
+    private WebTarget target = getWebTarget();
+
+    public static synchronized UserApi getInstance() {
+        if (instance == null) {
+            instance = new UserApi();
+        }
+        return instance;
+    }
+
+    private UserApi() {
+    }
+
+    private WebTarget getWebTarget() {
         ClientConfig config = new ClientConfig();
         Client client = ClientBuilder.newClient(config);
         return client.target(Main.URL);
     }
 
-    public static void testCreate(User user) {
-        WebTarget target = getWebTarget();
-        Response response = target.request().post(Entity.entity(user, MediaType.APPLICATION_JSON), Response.class);
-        System.out.println(response.getStatus());
-        System.out.println(response.readEntity(String.class));
+    public Response testCreate(User user) {
+        return target.request().post(Entity.entity(user, MediaType.APPLICATION_JSON), Response.class);
     }
 
-    public static void testGetList() {
-        WebTarget target = getWebTarget();
-        String response = target.request().accept(MediaType.APPLICATION_JSON).get(String.class);
-        System.out.println(response);
+    public Response testGetList() {
+        return target.request().accept(MediaType.APPLICATION_JSON).get(Response.class);
     }
 
-    public static void testGet(String id) {
-        WebTarget target = getWebTarget();
-        User user = target.path(id).request().accept(MediaType.APPLICATION_JSON).get(User.class);
-        System.out.println(user);
+    public Response testGet(String id) {
+        return target.path(id).request().accept(MediaType.APPLICATION_JSON).get(Response.class);
     }
 
-    public static void testUpdate(User user) {
-        WebTarget target = getWebTarget();
-        Response response = target.request().put(Entity.entity(user, MediaType.APPLICATION_JSON), Response.class);
-        System.out.println(response.getStatus());
-        System.out.println(response.readEntity(String.class));
+    public Response testUpdate(User user) {
+        return target.request().put(Entity.entity(user, MediaType.APPLICATION_JSON), Response.class);
     }
 
-    public static void testDeleteAll(String path) {
-        WebTarget target = getWebTarget();
-        Response response = target.path(path).request().delete(Response.class);
-        System.out.println(response.getStatus());
-        System.out.println(response.readEntity(String.class));
+    public Response testDeleteAll(String path) {
+        return target.path(path).request().delete(Response.class);
     }
 
-//    public static void testDelete() {
-//        WebTarget target = getWebTarget();
-//        User user = new User(4,"Alexoff", 34);
-//        Response response1 = target.path("4").request().get();
-//        Response response = target.request().accept(MediaType.APPLICATION_JSON).delete();
-//        System.out.println(response);
-//    }
+    public Response testDelete(User user) {
+        return target.request().method("DELETE", Entity.entity(user, MediaType.APPLICATION_JSON), Response.class);
+    }
 }
